b5e7a88cc42a7c3a382e4f134edfbafa112e0424
Sheree Nicholson <sdnicholson8@gmail.com>
2023-10-10 21:39:01 -0400

    attempting to make another type of slideshow

---
+++ my-portfolio/src/components/Slider.js
@@ -0,0 +1,15 @@
+//import {useState, useEffect, useRef} from "react";
+import Capstone from "../assets/Capstone.jpg"
+import Lucky from "../assets/Lucky.jpg"
+import Game from "../assets/Game.jpg"
+
+const
+
+function Slider () {
+  return(
+    <div className=".slideshow">
+    </div>
+  );
+};
+
+export default Slider;
--- my-portfolio/src/components/Slideshow.js
+++ my-portfolio/src/components/Slideshow.js
@@ -1,50 +1,57 @@
 import {useState, useEffect, useRef} from "react";
 
-
-const colors = [{url: "assets/Capstone.jpg"}, "#00C49F", "#FFBB28", "#FFBB28"]
+const colors = ["#088FE", "00C49F", "#FFBB28"];
 const delay = 2500;
 
-function Slideshow () {
+function Slideshow() {
   const [index, setIndex] = useState(0);
   const timeoutRef = useRef(null);
 
-function resetTimeout() {
-  if (timeoutRef.current) {
-    clearTimeout(timeoutRef.current)
-  };
-};
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
 
-useEffect(() => {
-  resetTimeout();
-  timeoutRef.current = setTimeout(
-    () =>
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () => 
       setIndex((prevIndex) =>
         prevIndex === colors.length - 1 ? 0 : prevIndex + 1),
-    delay
+        delay);
+      
+      return () => {
+        resetTimeout();
+      }
+  }, [index]);
+
+  return (
+    <div className="slideshow">
+      <div className="slider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)`}}>
+        {colors.map((backgroundColor, index) => (
+          <div
+            className="slide"
+            key={index}
+            style={{backgroundColor}}
+            ></div>
+        ))}
+      </div>
+
+      <div className="showDots">
+        {colors.map((_, idx) => (
+          <div
+          key={idx}
+          className={`showDot${index === idx ? "active" : ""}`}
+          onClick={() => {
+            setIndex(idx);
+          }}
+          ></div>
+        ))}
+      </div>
+    </div>
   );
-    return () =>{
-      resetTimeout();
-    };
-}, [index]);
-
-return (
-<div className="slideshow">
-  <div className="slider"
-    style={{transform: `translate3d(${-index * 100}%, 0, 0)`}}>
-    {colors.map((backgroundColor, index) =>(
-      <div className="slide" key={index} style={{backgroundColor}} />
-    ))}
-  </div>
-
-  <div className="showDots">
-    {colors.map((_, idx) => (
-      <div key={idx} className={`showDot${index === idx ? "active": ""}`}
-      onClick={() => { setIndex(idx);}}/>
-    ))}
-  </div>
-</div>
-  )
-};
+}
 
 
 export default Slideshow;
