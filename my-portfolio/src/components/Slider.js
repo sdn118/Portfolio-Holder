3225ce747e9b7a2fb738eb13c0ab8ed3b055e2e8
Sheree Nicholson <sdnicholson8@gmail.com>
2023-10-12 12:43:56 -0400

    Continuing to edit potential other slideshow

--- my-portfolio/src/components/Slider.js
+++ my-portfolio/src/components/Slider.js
@@ -3,11 +3,38 @@ import Capstone from "../assets/Capstone.jpg"
 import Lucky from "../assets/Lucky.jpg"
 import Game from "../assets/Game.jpg"
 
-const
+const slides =[
+  {
+    image: {Capstone},
+    title: "Little Lemon",
+  },
 
-function Slider () {
+  {
+    image: {Lucky},
+    title: "Lucky Shrub",
+  },
+
+  {
+    image: {Game},
+    title: "Rock, Paper, Scissors",
+  }
+]
+
+function Slider ({slides}) {
   return(
-    <div className=".slideshow">
+    <div className="slideshow">
+      <div className="show-wrapper">
+        {slides.map((slides) =>{
+          return <div key={index} className="slide-card">
+            <img src={image.imgage} alt="" />
+            <div className="show-overlay">
+              <h2 className="slide-title">{imgage.title}</h2>
+            </div>
+          </div>;
+          
+        })}
+      </div>
+      
     </div>
   );
 };
