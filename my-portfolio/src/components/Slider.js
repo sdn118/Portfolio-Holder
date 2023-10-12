c34cb4cc9b8262c09f0f03050fb3bccfba879058
Sheree Nicholson <sdnicholson8@gmail.com>
2023-10-12 18:04:48 -0400

    Finishing slider code to see functionality

--- my-portfolio/src/components/Slider.js
+++ my-portfolio/src/components/Slider.js
@@ -1,4 +1,4 @@
-//import {useState, useEffect, useRef} from "react";
+import {useState, useEffect, useRef} from "react";
 import Capstone from "../assets/Capstone.jpg"
 import Lucky from "../assets/Lucky.jpg"
 import Game from "../assets/Game.jpg"
@@ -21,20 +21,52 @@ const slides =[
 ]
 
 function Slider ({ slides }) {
+  const [current, setCurrent] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(true);
+  let timeOut = null
+
+  useEffect(()=> {
+    timeOut =
+    autoPlay && 
+    setTimeout(()=>{}, 2500) 
+  })
   return(
-    <div className="slideshow">
+    <div className="slideshow"
+    onMouseEnter={()=>{
+      setAutoPlay(false);
+      clearTimeout(timeOut)
+    }}
+    onMouseLeave={()=>{
+      setAutoPlay(true);
+    }}>
       <div className="show-wrapper">
         {slides.map((image, index) =>{
-          return <div key={index} className="slide-card">
+          return <div 
+            key={index} 
+            className=
+            {index == current ? "slide-card slide-card-active"
+               : "slide-card"}>
             <img src={image.image} alt="" />
             <div className="show-overlay">
               <h2 className="slide-title">{image.title}</h2>
             </div>
           </div>;
           
-        })}
+        })};
+
+        <div className="slide-dots">
+          {slides.nao((_, index) => {
+            return <div
+            key={index} 
+            className={
+              index == current ? "slide-dots slide-dots-active"
+               : "slide-dots"}
+               onClick={() => setCurrent(index)}>
+
+            </div>
+          })}
+        </div>
       </div>
-      
     </div>
   );
 };
