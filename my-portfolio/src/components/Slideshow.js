16e0d0e6a468233b9f3643fd312ae58bd2fa5bac
Sheree Nicholson <sdnicholson8@gmail.com>
2023-10-16 12:43:01 -0400

    Adding slides file

--- my-portfolio/src/components/Slideshow.js
+++ my-portfolio/src/components/Slideshow.js
@@ -1,45 +1,27 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// src/App.js
+import ImageSlider from "./ImageSlider";
 import Capstone from "../assets/Capstone.jpg";
 import Lucky from "../assets/Lucky.jpg";
 import Game from "../assets/Game.jpg";
 
-const slideshow = ({images}) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-  };
-}
-
-function Slideshow () {
+function App() {
   const images = [
     Capstone,
     Lucky,
     Game,
+    Capstone,
+    Lucky,
+    Game,
+    // Add more image URLs here
   ];
+  
+  return (
+    <div className="App">
+      <div style={{padding: "10px"}}>
+        <ImageSlider images={images} />
+      </div>
+    </div>
+  );
+}
 
-
-
-
-return (
-  <div className="image-slider">
-    <Slider {...settings}>
-      {images.map((image, index) => (
-        <div key={index}>
-          <img src={image} alt={`Slide ${index}`} />
-        </div>
-      ))}
-    </Slider>
-  </div>
-);
-};
-
-export default Slideshow;
+export default App;
